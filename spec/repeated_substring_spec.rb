6723f61c920e94ed4f6ba30c9ccecc88e92fe75a
Janis-Leuenberger <50821376+Janis-Leuenberger@users.noreply.github.com>
2019-09-11 19:19:55 +0200

    My Solution

--- spec/repeated_substring_spec.rb
+++ spec/repeated_substring_spec.rb
@@ -4,10 +4,12 @@ require 'repeated_substring'
 RSpec.describe 'repeated_substring' do
   it 'is an example' do
     rs = RepeatedSubstring.new
-    expect(rs.find_repeated_substring('banana')).to eq(3)
+    expect(rs.find_repeated_substring('banana')).to eq('an')
   end
 
-  xit 'find the most repeated substring for every line of a file' do
-    expect(RepeatedSubstring.new.find_repeated_substring_file('data/sample.txt')).to eq(['an', 'NONE', 'NONE', 'oo'])
+  #https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/
+  #I used a modified version of this python script to find the correct solution
+  it 'find the most repeated substring for every line of a file' do
+    expect(RepeatedSubstring.new.find_repeated_substring_file('data/sample.txt')).to eq(["aqca", "NONE", "xf", "nr", "wmfl", "edpl", "pdbyc", "NONE", "exng", "zu", "la", "NONE", "ztjh", "m u", "yhfkc", "du ", "zpvxel", "coailb", "NONE", "lish", "dvro", "a ", "zf", "wyoup", "ekpu", "axn", "lhzpx", "melgn", "NONE", "ttllv", "cj", "NONE", "jhrda", "mp", "NONE", "NONE", "NONE", " o ", "low " , "ny"])
   end
 end
